06a3fd5f671ac6ed39affc54745e4d3cda3d9bb4
Rob Tilling <robert.tilling@xrtsoft.net>
2026-06-27 18:29:40 +0100

    Updated global supp list

--- tests/PowerApps.CLI.IntegrationTests/GlobalSuppressions.cs
+++ tests/PowerApps.CLI.IntegrationTests/GlobalSuppressions.cs
@@ -1,8 +1,8 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
+// This file is used by Code Analysis to maintain SuppressMessage
 // attributes that are applied to this project.
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
 
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("Style", "IDE0305:Simplify collection initialization", Justification = "<Pending>", Scope = "member", Target = "~M:PowerApps.CLI.IntegrationTests.ProcessManage.ProcessManageTests.InitializeAsync~System.Threading.Tasks.Task")]
+[assembly: SuppressMessage("Style", "IDE0305:Simplify collection initialization", Justification = "Code less readable if this is applied")]
